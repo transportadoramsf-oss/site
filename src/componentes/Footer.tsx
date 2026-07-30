780c3891f82e86c44d501a974082d9390e7d29a1
Meke04 <luismeke0@gmail.com>
2026-07-30 16:22:02 -0300

    style: pagina principal para monitor pronta

--- src/componentes/Footer.tsx
+++ src/componentes/Footer.tsx
@@ -1,40 +1,38 @@
+import { Truck } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export function Footer() {
   return (
     <footer className="bg-gray-800 text-white">
-      <div className="w-[90%] max-w-7xl mx-auto">
-        <div className="flex justify-between w-full">
+      <div className="w-[90%] max-w-7xl mx-auto px-4 p-4">
+        <div className="flex justify-between items-center w-full">
           <div>
-            <div className="flex p-2 gap-2">
-              <Image src="/a.webp" alt="imagem" height={40} width={40} />
-              <div>
-                <h1>Nome</h1>
-                <h1>SubNome</h1>
-              </div>
+            <div className="flex gap-2  shrink-0">
+              <Truck size={40} className="" />
+              <h1 className="text-sm font-bold">Consultoria<br/> & Logistica.</h1>
             </div>
-            <p>soluções completas em transporte e logistica para todo o Brasil.</p>
+            <p className="text-xs pt-1">soluções completas em transporte<br/> & logistica para todo o Brasil.</p>
           </div>
           <div>
-            <h1>NAVEGAÇÃO</h1>
-            <div className="flex flex-col">
+            <h1 className="text-lg font-semibold">NAVEGAÇÃO</h1>
+            <div className="flex flex-col text-sm font-medium text-gray-100">
               <Link href="/">Inicio</Link>
-              <Link href="/">Sobre</Link>
-              <Link href="/">Contato</Link>
+              <Link href="/Sobre">Sobre</Link>
+              <Link href="/Contato">Contato</Link>
             </div>
           </div>
           <div>
-            <h1>SERVIÇOS</h1>
-            <div>
+            <h1 className="text-lg font-semibold">SERVIÇOS</h1>
+            <div className="flex flex-col text-sm font-medium text-gray-100">
               <h1>Dedicada</h1>
               <h1>Fracionada</h1>
               <h1>Especiais</h1>
             </div>
           </div>
           <div>
-            <h1>CONTATO</h1>
-            <div className="flex flex-col">
+            <h1 className="text-lg font-semibold">CONTATO</h1>
+            <div className="flex flex-col text-sm font-medium text-gray-100">
               <Link href="/">NUMERO</Link>
               <Link href="/">EMAIL</Link>
               <Link href="/">ENDEREÇO</Link>
@@ -43,8 +41,8 @@ export function Footer() {
         </div>
       </div>
         <hr className="border-gray-700" />
-        <div className="max-w-7xl mx-auto py-1 px-4 sm:px-6 lg:px-8">
-          <p className="text-center">&copy; 2026 My App. Todos os direitos reservados.</p>
+        <div className="max-w-7xl mx-auto py-1">
+          <p className="text-center">&copy; 2026 MSF Consultoria & Logistica. Todos os direitos reservados.</p>
         </div>
     </footer>
   );
--- src/componentes/NavBar.tsx
+++ src/componentes/NavBar.tsx
@@ -1,18 +1,27 @@
+"use client";
+
+import { Truck } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 
 export function NavBar() {
+
+  const pathname = usePathname();
+
   return (
     <nav className="bg-gray-100 text-gray-900">
       <div className="w-[90%] max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="shrink-0">
-            <h1 className="text-xl font-bold">My App</h1>
+          <div className="flex gap-2  shrink-0">
+            <Truck size={40} className="text-black" />
+            <h1 className="text-sm font-bold">Consultoria<br/> & Logistica.</h1>
           </div>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
-              <Link href="/" className="py-2 rounded-md text-sm font-medium">Inicio</Link>
-              <Link href="/Sobre" className="px-3 py-2 rounded-md text-sm font-medium">Sobre</Link>
-              <Link href="/Contato" className="px-3 py-2 rounded-md text-sm font-medium">Contato</Link>
+              <Link href="/" className={`px-3 py-2 rounded-md ${pathname === "/" ? "text-base font-bold underline underline-offset-3 decoration-3" : "text-sm font-medium"}`}>Inicio</Link>
+              <Link href="/Sobre" className={`px-3 py-2 rounded-md ${pathname === "/Sobre" ? "text-base font-bold underline underline-offset-3 decoration-3" : "text-sm font-medium"}`}>Sobre</Link>
+              <Link href="/Contato" className={`px-3 py-2 rounded-md ${pathname === "/Contato" ? "text-base font-bold underline underline-offset-3 decoration-3" : "text-sm font-medium"}`}>Contato</Link>
             </div>
           </div>
         </div>
